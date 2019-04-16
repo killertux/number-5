83d77160302634bb7fdc27a2b2c5ee22621ab560
killertux <bruno.canguru@hotmail.com>
2019-04-15 23:57:27 -0300

    Add procfile

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(proc_macro_hygiene, decl_macro, custom_attribute)]
 #[macro_use] extern crate rocket;
+use rocket::Config;
+use std::env;
 
 mod phrases;
 mod where_should_we_eat;
@@ -9,8 +11,17 @@ fn hello() -> &'static str {
     "Hello, world!"
 }
 
+fn configure() -> Config {
+    let mut config = Config::active().expect("could not load configuration");
+    if let Ok(port_str) = env::var("PORT") {
+        let port = port_str.parse().expect("could not parse PORT");
+        config.set_port(port);
+    }
+    config
+}
+
 fn main() {
-    rocket::ignite()
+    rocket::custom(configure())
         .mount("/", routes![where_should_we_eat::get_place_to_eat])
         .register(catchers![phrases::get_phrase])
         .launch();
