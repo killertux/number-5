8dfc233b4cdecf9acf3494efc7c5ce6c06c4848f
Jomaro Rodrigues <jomaro.rodrigues@gmail.com>
2019-04-23 22:51:30 -0300

    makes it compile again and adds class to the code

--- src/weather/mod.rs
+++ src/weather/mod.rs
@@ -12,32 +12,18 @@ pub enum WeatherResponse {
         response: bool,
         timestamp: u32,
     },
-    Error {error: &'static str},
-}
-
-#[derive(Deserialize, Debug)]
-struct OpenWeatherResponse {
-    coord: Coord,
-    weather: [Weather; 1],
-    base: String,
-    main: Main,
-    visibility: i32,
-    wind: Wind,
-    clouds: Clouds,
-    dt: i32,
-    sys: Sys,
-    id: i32,
-    name: String,
-    cod: i32
+    Error {
+        message: &'static str
+    },
 }
 
 #[get("/is-it-raining")]
 pub fn is_it_raining() -> Json<WeatherResponse> {
     let url_request = create_request();
-    let response : WeatherResponse = match execute_request(url_request) {
-        Ok(open_weather_response) => code_to_raining_state(open_weather_response),
-        Err(_) => WeatherResponse::Error{error:"Open Weather is out of service!"},
-    };
+
+    let response = execute_request(url_request)
+                    .map(code_to_raining_state)
+                    .unwrap_or(WeatherResponse::Error{message:"Open Weather is out of service!"});
 
     Json(response)
 }
@@ -66,4 +52,65 @@ fn get_timestamp() -> u32 {
     SystemTime::now().duration_since(UNIX_EPOCH)
         .expect("Something very wrong happend!")
         .as_secs() as u32
+}
+
+
+#[derive(Deserialize, Debug)]
+struct OpenWeatherResponse {
+    coord: Coord,
+    weather: [Weather; 1],
+    base: String,
+    main: Main,
+    visibility: i32,
+    wind: Wind,
+    clouds: Clouds,
+    dt: i32,
+    sys: Sys,
+    id: i32,
+    name: String,
+    cod: i32
+}
+
+#[derive(Deserialize, Debug)]
+struct Coord {
+    lon: f32,
+    lat: f32,
+}
+
+#[derive(Deserialize, Debug)]
+struct Weather {
+    id: u32,
+    main: String,
+    description: String,
+    icon: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Main {
+    temp: f32,
+    pressure: i32,
+    humidity: i32,
+    temp_min: f32,
+    temp_max: f32,
+}
+
+#[derive(Deserialize, Debug)]
+struct Wind {
+    speed: f32,
+    deg: Option<i32>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Clouds {
+    all: i32,
+}
+
+#[derive(Deserialize, Debug)]
+struct Sys {
+    r#type: i32,
+    id: i32,
+    message: f32,
+    country: String,
+    sunrise: i32,
+    sunset: i32,
 }
