40c0a3477a75085cd8e74d10f99cba39144cfcd8
Bruno Clemente <bruno.canguru@hotmail.com>
2019-05-23 10:34:17 -0300

    Add more endpoints to discover future places to eat

--- src/where_should_we_eat/mod.rs
+++ src/where_should_we_eat/mod.rs
@@ -10,8 +10,36 @@ pub struct PlaceToEat {
     place_to_eat: String,
 }
 
+#[derive(Serialize)]
+pub struct PlaceToEatDay {
+    days_from_today: u32,
+    place_to_eat: PlaceToEat,
+}
+
 #[get("/where-should-we-eat")]
 pub fn get_place_to_eat() -> Json<PlaceToEat> {
+    let offset = 0;
+    Json(choose_place_to_eat(offset))
+}
+
+#[get("/where-should-we-eat/<offset>")]
+pub fn get_place_to_eat_with_offset(offset: u32) -> Json<PlaceToEat> {
+    Json(choose_place_to_eat(offset))
+}
+
+#[get("/where-should-we-eat-in-the-next-days/<days>")]
+pub fn get_place_to_eat_in_the_next_days(days: u32) -> Json<Vec<PlaceToEatDay>> {
+    let mut next_days: Vec<PlaceToEatDay> = Vec::<PlaceToEatDay>::new();
+    for day in 0..days {
+        next_days.push(PlaceToEatDay{
+            days_from_today: day,
+            place_to_eat: choose_place_to_eat(day),
+        })
+    }
+    Json(next_days)
+}
+
+fn choose_place_to_eat(offset: u32) -> PlaceToEat {
     let premium_places = vec![
         "Madero",
         "Marbo",
@@ -42,24 +70,23 @@ pub fn get_place_to_eat() -> Json<PlaceToEat> {
     ];
     let places = [(premium_places, 1), (normal_places, 19)];
     let distribution = WeightedIndex::new(places.iter().map(|place| place.1)).unwrap();
+    let chosen_list = &places[distribution.sample(&mut get_rnd(&offset))].0;
 
-    let chosen_list = &places[distribution.sample(&mut get_rnd())].0;
-
-    Json(PlaceToEat{
-        place_to_eat: get_from_random_list(chosen_list)
-    })
+    PlaceToEat{
+        place_to_eat: get_from_random_list(chosen_list, &offset)
+    }
 }
 
-fn get_from_random_list(places: &[&str]) -> String {
-    places.choose(&mut get_rnd())
+fn get_from_random_list(places: &[&str], offset: &u32) -> String {
+    places.choose(&mut get_rnd(& offset))
             .unwrap_or(&"Rand error!")
             .to_string()
 }
 
-fn get_rnd() -> StdRng {
+fn get_rnd(offset: &u32) -> StdRng {
     let super_seed = 42;
     let mut rnd = StdRng::seed_from_u64(super_seed);
-    for _i in 0..get_days_since_my_birth() {
+    for _i in 0..(get_days_since_my_birth() + offset) {
         rnd.next_u32();
     }
     rnd
